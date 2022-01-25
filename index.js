7ac5c75264cc5e01131993e81fa84ee49eb00d4c
Yu Tae Young <neovansoarer@gmail.com>
2022-01-25 15:15:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,23 +7,26 @@ const resultList = document.querySelector('#resultList')
 
 form.addEventListener('submit', function (event) {
   event.preventDefault()
+  resultList.innerText = null
+
   const N = parseInt(allCount.value)
   const T = parseInt(teamCount.value)
   const absentNumbers = absents.value.split(' ').map(Number)
-  const lefts = N % T
+  const lefts = (N-absentNumbers.length) % T
   const students = _.shuffle(_.range(1, N+1))
 
   const currentStudents = students.filter(student => !absentNumbers.includes(student))
   const teamNo = parseInt((N - absentNumbers.length) / T)
-    
+  
   for (let i=0; i<teamNo; i++) {
+    console.log(lefts)
     const teammates = currentStudents.slice(i*T, (i+1)*T)
-
     if (i < lefts) {
-      teammates.push(currentStudents[currentStudents.length-i-1])
+      teammates.push(currentStudents[currentStudents.length - i-1])
     }
+    console.log(teammates, currentStudents)
     
-    console.log(teammates.sort((x, y) => x - y))
+    // teammates.sort((x, y) => x - y)
     resultList.appendChild(createTeamInfo(i+1, teammates))
   }
 })
