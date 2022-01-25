66280ebaef773f8deb21364181f3a0e53df00502
Yu Tae Young <neovansoarer@gmail.com>
2022-01-25 14:56:07 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+const form = document.querySelector('#form')
+
+const allCount = document.querySelector('#allCount')
+const teamCount = document.querySelector('#teamCount')
+const absents = document.querySelector('#absents')
+const resultList = document.querySelector('#resultList')
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault()
+  const N = parseInt(allCount.value)
+  const T = parseInt(teamCount.value)
+  const absentNumbers = absents.value.split(' ').map(Number)
+  const lefts = N % T
+  const students = _.shuffle(_.range(1, N+1))
+
+  const currentStudents = students.filter(student => !absentNumbers.includes(student))
+  const teamNo = parseInt((N - absentNumbers.length) / T)
+    
+  for (let i=0; i<teamNo; i++) {
+    const teammates = currentStudents.slice(i*T, (i+1)*T)
+
+    if (i < lefts) {
+      teammates.push(currentStudents[currentStudents.length-i-1])
+    }
+    
+    console.log(teammates.sort((x, y) => x - y))
+    resultList.appendChild(createTeamInfo(i+1, teammates))
+  }
+})
+
+function createTeamInfo(teamNo, teammates) {
+  const li = document.createElement('li')
+  li.classList.add('list-group-item')
+  li.innerText =`${teamNo}조: ${teammates.join(', ')}` 
+  return li
+}
